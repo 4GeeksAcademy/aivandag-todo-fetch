2f52886eb702c5faa9df4e2727390369c6249d67
aivandag <aivan.dev7@gmail.com>
2025-05-16 01:55:12 +0000

    TodoFetch2.0

--- src/js/components/Todo.jsx
+++ src/js/components/Todo.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/Todo.css";
+
+const apiUrl = "https://playground.4geeks.com/todo"
+const userName = "aivandag"
 
 const inicialEstadoTarea = {
     label: "",
@@ -23,11 +26,75 @@ const Todo = () => {
 
         if (tarea.label.trim() === "") return; {
 
-            setListaTarea([...listaTarea, tarea]);
+            addTask()
             setTarea(inicialEstadoTarea);
         }
     };
 
+    const createUser = () => {
+        fetch(`${apiUrl}/users/${userName}`, { method: "POST" })
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }
+
+    const getTasks = () => {
+        fetch(`${apiUrl}/users/${userName}`)
+            .then(response => {
+                if (response.status == 404) {
+                    createUser()
+                    return false
+                }
+                return response.json()
+            })
+            .then(data => {
+                if (data) {
+                    setListaTarea(data.todos)
+                }
+            })
+    }
+
+    const addTask = () => {
+        fetch(`${apiUrl}/todos/${userName}`, {
+            method: "POST",
+            body: JSON.stringify(tarea),
+            headers: { "Content-Type": "application/json" }
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+
+            })
+            .then(data => {
+                if (data) {
+                    setListaTarea([...listaTarea, data])
+                }
+            })
+    }
+
+
+   const deleteTask = (id) => {
+    fetch(`${apiUrl}/todos/${id}`, {
+        method: "DELETE"
+    })
+    .then(response => {
+        if (response.ok) {
+         
+            setListaTarea(listaTarea.filter((item) => item.id !== id));
+        } else {
+            console.error("Error al eliminar la tarea.");
+        }
+    })
+    .catch(err => console.error("Error en la solicitud DELETE:", err));
+};
+
+
+
+
+
+    useEffect(() => {
+        getTasks()
+    }, [])
 
     return (
 
@@ -56,13 +123,13 @@ const Todo = () => {
                             className="lista-item"
                         >
                             {item.label}
-                            
+
                             <span
                                 className="delete"
-                                
+
                                 style={{ cursor: "pointer" }}
                                 onClick={() =>
-                                    setListaTarea(listaTarea.filter((_, i) => i !== index))
+                                    deleteTask(item.id)
                                 }
                             >
                                 ❌
